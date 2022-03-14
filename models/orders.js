559884348b5bf0ebfcee859b48c704b4febfa5bb
eason830 <orz0830@gmail.com>
2022-03-14 19:30:28 +0800

    上傳

---
+++ models/orders.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+// 訂單
+const orderSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.ObjectId,
+    // ref代表這個欄位的資料是users來的
+    ref: 'users'
+  },
+  products: {
+    type: [
+      {
+        product: {
+          type: mongoose.ObjectId,
+          ref: 'products',
+          required: [true, '缺少商品 ID']
+        },
+        quantity: {
+          type: Number,
+          required: [true, '缺少商品數量']
+        }
+      }
+    ]
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+}, { versionKey: false })
+
+export default mongoose.model('orders', orderSchema)
---
+++ models/products.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose'
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, '商品名不能為空']
+  },
+  price: {
+    type: Number,
+    min: [0, '價格格式不正確'],
+    required: [true, '商品價格不能為空']
+  },
+  description: {
+    type: String
+  },
+  image: {
+    type: String
+  },
+  // 是否上架
+  sell: {
+    type: Boolean,
+    default: false
+  },
+  // 商品的分類(靜態固定那幾類)
+  category: {
+    type: String,
+    //  enum 值只能是陣列裡的類別
+    enum: {
+      values: ['飾品', '皮件', '鞋子'],
+      message: '商品分類不存在'
+    }
+  }
+}, { versionKey: false })
+
+export default mongoose.model('products', productSchema)
